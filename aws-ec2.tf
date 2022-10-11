7fc70539ba0d260d15a4b698a74568bd24fec7f9
jankamotor <juancarlos.rivas.alcon@ucreativa.com>
2022-10-11 17:06:02 -0600

    Modificacion archivo aws-ec2.tf

--- aws-ec2.tf
+++ aws-ec2.tf
@@ -8,6 +8,11 @@ resource "aws_instance" "first_ec2" {
     user_data = file("./files/server-script.sh")
 
     security_groups = [aws_security_group.web_traffic.name]
+
+    network_interface {
+      network_interface_id = aws_network_interface.web-eth0.id
+      device_index         = 0
+  }
 }
 
 output "public_ip" {
